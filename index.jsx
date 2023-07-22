b5a66c9ec70815ee3dcc24730d1102312beb74d3
smwasifzamanomee <wasifbdjsr@gmail.com>
2023-07-22 11:36:49 +0600

    api fetch using redux

--- index.jsx
+++ index.jsx
@@ -1,53 +1,85 @@
-const { createStore } = require("redux");
+const { createStore, applyMiddleware } = require("redux");
+const { default: thunk } = require("redux-thunk");
+const axios = require("axios");
 
 //constants
-const Increment = "Increment";
-const Decrement = "Decrement";
-const Reset = "Reset";
-//state
-const initialState = 0;
+const TODOS_REQUEST = 'TODOS_REQUEST';
+const TODOS_SUCCESS = 'TODOS_SUCCESS';
+const TODOS_FAILD = 'TODOS_FAILD';
+const API_URL = 'https://jsonplaceholder.typicode.com/todos';
 
-//action creators
-const increment = () => {
-    return {
-        type: Increment,
-    };
+//state
+const initialState = {
+    todos: [],
+    isloading: false,
+    error: null
 }
-const decrement = () => {
+
+//action
+const todosRequest = () => {
     return {
-        type: Decrement,
-    };
+        type: TODOS_REQUEST
+    }
 }
-const reset = () => {
+const todosSuccess = (todos) => {
     return {
-        type: Reset,
-    };
+        type: TODOS_SUCCESS,
+        payload: todos
+    }
+}
+const todosFaild = (error) => {
+    return {
+        type: TODOS_FAILD,
+        payload: error
+    }
 }
 
 //reducer
-const counterReducer = (state = initialState, action) => {
+const todosReducer = (state = initialState, action) => {
     switch (action.type) {
-        case Increment:
-            return state + 1;
-        case Decrement:
-            return state - 1;
-        case Reset:
-            return initialState;
+        case TODOS_REQUEST:
+            return {
+                ...state,
+                isloading: true
+            }
+        case TODOS_SUCCESS:
+            return {
+                ...state,
+                todos: action.payload,
+                isloading: false
+            }
+        case TODOS_FAILD:
+            return {
+                ...state,
+                error: action.payload,
+                isloading: false
+            }
         default:
             return state;
     }
 }
 
+// async action
+const fetchData = () => {
+    return (dispatch) => {
+        dispatch(todosRequest());
+        axios.get(API_URL)
+            .then((response) => {
+                const todos = response.data;
+                dispatch(todosSuccess(todos));
+            })
+            .catch((error) => {
+                dispatch(todosFaild(error.message));
+            })
+    }
+}
+
 //store
-const store = createStore(counterReducer);
+const store = createStore(todosReducer, applyMiddleware(thunk));
 
 store.subscribe(() => {
     console.log(store.getState());
 }
-);
+)
 
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(reset());
-
+store.dispatch(fetchData());
